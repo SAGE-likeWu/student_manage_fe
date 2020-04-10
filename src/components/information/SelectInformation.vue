15ad91689e03a722a7eccbff247d07da9d00c228
liaodg <626933286@qq.com>
2020-04-10 21:50:41 +0800

    22

--- src/components/information/SelectInformation.vue
+++ src/components/information/SelectInformation.vue
@@ -3,7 +3,6 @@
 
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
       <el-menu :default-openeds="['1', '3']">
-        <!--<el-menu-item ><i class="el-icon-search"></i>查询方式</el-menu-item>-->
         <el-menu-item-group>
 
           <el-submenu index="1">
@@ -143,6 +142,7 @@
           this.$axios.post(`waybill/find`,this.waybill).then(res => {
             if (res.data.code == 200){
               this.tableData = res.data.data.items
+              this.pagin.total = res.data.data.totalNum
             }else {
               this.$notify.error({
                 title: '错误',
